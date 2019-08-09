6b0142f4e55ac054efc0523552fda265fe0119b8
Junho Park <jhpark870708@daum.net>
2019-08-09 20:12:49 +0900

    add java-practive04

--- src/prob03/DropShip.java
+++ src/prob03/DropShip.java
@@ -1,9 +1,6 @@
 package prob03;
 
-public class DropShip {
-	// 현재 위치
-	private int x;
-	private int y;
+public class DropShip extends Unit{
 	
 	void load() {
 		/* 선택된 대상을 태운다.*/ 
@@ -13,11 +10,4 @@ public class DropShip {
 		/* 선택된 대상을 내린다.*/ 
 	}
 	
-	void move(int x, int y) {
-		/* 지정된 위치로 이동 */
-	}
-
-	void stop() {
-		/* 현재 위치에 정지 */
-	}	
 }
--- src/prob05/Prob05.java
+++ src/prob05/Prob05.java
@@ -7,6 +7,11 @@ public class Prob05 {
 
 		base.service("낮");
 		base.service("밤");
-		base.service("오후"); 
+		base.service("오후");
+		
+//		낮에는 열심히 일하자!
+//		night
+//		오후도 낮과 마찬가지로 일해야 합니다.
+
 	}
 }
